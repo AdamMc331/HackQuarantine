bc1205c55c9a66fd6c4d6b243989a14a0f82b20a
Adam McNeilly <amcneilly331@gmail.com>
2020-03-28 14:57:19 -0400

    Adding student database. (#1)

--- app/src/main/java/com/adammcneilly/hackquarantinedemo/Student.kt
+++ app/src/main/java/com/adammcneilly/hackquarantinedemo/Student.kt
@@ -1,5 +1,13 @@
 package com.adammcneilly.hackquarantinedemo
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "students")
 data class Student(
+    @PrimaryKey(autoGenerate = true)
+    val studentId: Long = 0,
+    @ColumnInfo(name = "student_name")
     val studentName: String
 )
